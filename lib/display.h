a275c167dbdfc7083015392c09f79a65035d7c97
Camila_RG <camilaramosgomes@gmail.com>
2025-02-22 08:41:12 -0300

    Bibliotecas para display adicionadas

---
+++ lib/display.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pico/stdlib.h"
+#include "hardware/adc.h"
+#include "hardware/i2c.h"
+#include "lib/ssd1306.h"
+#include "lib/font.h"
+
+#define I2C_PORT i2c1
+#define I2C_SDA 14
+#define I2C_SCL 15
+#define endereco 0x3C
+#define JOYSTICK_X_PIN 26  // GPIO para eixo X
+#define JOYSTICK_Y_PIN 27  // GPIO para eixo Y
+#define JOYSTICK_PB 22 // GPIO para botão do Joystick
+#define Botao_A 5 // GPIO para botão A
